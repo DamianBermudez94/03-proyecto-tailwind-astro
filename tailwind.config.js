b189fe9f1a01dbd4848008a1d97b0cd59696dd9d
DamianBermudez94 <bermudezdamian7@gamil.com>
2025-02-15 18:20:05 -0300

    se creo la version movil y destok del header y el hero(pagina de inicio)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-    content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
-    theme: {
-      extend: {},
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}',
+    './public/index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "White-Name": "hsl(0deg 0% 95.29% / 59.61%)",
+        "White-Principal": "hsl(0deg 0% 87.45%)",
+        "Violet-text": "hsl(260.15deg 100% 73.14%)",
+
+      },
+      backgroundImage: {
+        'Violet-background': "linear-gradient(135deg, rgba(164, 118, 255, 0.5) 25%, rgba(238, 229, 255, 0.5), rgba(164, 118, 255, 0.5) 75%)"
+      }
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
