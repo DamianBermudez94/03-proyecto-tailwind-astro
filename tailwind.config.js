ad24fd4d49fa1390dd85a18062abff6a22ebf8f3
DamianBermudez94 <bermudezdamian7@gamil.com>
2025-02-19 18:52:33 -0300

    se empezo a diseñar y darle funcionalidad al componente PROYECTOS

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,9 @@ module.exports = {
         "Violet-text": "hsl(260.15deg 100% 73.14%)",
         "Background-body":"#101010",
         'Violet-background': "rgba(164, 118, 255, 0.3)",
-        "Background-linea":"#ffffff10"
+        "Background-linea":"#ffffff10",
+        "Background-icon" :"#f3f3f310",
+        "Color-icon":"#f3f3f398"
 
       },
       backgroundImage: {
